f0e3b1f7616c4ea0b8f3bc928c165c8d852c8c29
Chu <zhichu1997@gmail.com>
2020-09-29 16:59:45 +0800

    Remove redundant code

--- MyNet.go
+++ MyNet.go
@@ -9,23 +9,6 @@ package MyNet
 #include <arpa/inet.h>
 #include <stddef.h>
 
-int AddFunction(int a , int b){
-	return a+b;
-}
-int create_socket(){
-	int fd;
-	fd = socket(AF_INET,SOCK_DGRAM,0);
-	return fd;
-}
-char* create_buffer(int buflen){
-	char* buffer;
-	buffer = malloc(buflen*sizeof(char));
-	return buffer;
-}
-
-void printString(const char* s) {
-    printf("%s", s);
-}
 */
 import "C"
 import (
@@ -48,11 +31,6 @@ type MyConn struct {
 	fd C.int
 	Claddr C.struct_sockaddr_in
 	Craddr C.struct_sockaddr_in
-}
-//no use
-type MyUDPAddr struct{
-	IP net.IP
-	port int
 }
 
 func ListenUDP(network string,laddr *net.UDPAddr) (*MyConn,error){
@@ -84,28 +62,14 @@ func (conn *MyConn) ReadFromUDP(b []byte)(int,*net.UDPAddr,error){
 	var recv_length C.long
 	len_address := C.uint(unsafe.Sizeof(addr))
 	fmt.Printf("This is readfromudp\n")
-	//create memory space
-	var buffer string
-	buffer_ptr := C.CString(buffer)
-	defer C.free(unsafe.Pointer(buffer_ptr))
-	//recv_length = C.recvfrom(conn.fd,unsafe.Pointer(buffer_ptr),BUF_LEN,0,(*C.struct_sockaddr)(unsafe.Pointer(&addr)),&len_address)
 	recv_length = C.recvfrom(conn.fd,unsafe.Pointer(&b[0]),BUF_LEN,0,(*C.struct_sockaddr)(unsafe.Pointer(&addr)),&len_address)
 	//deal with err (get-1)
 	if int(recv_length) == -1{
 		err:=errors.New("fail to receive")
 		return -1, nil, err
 	}
-	/*
-	//get the remote address and put it into UDPAddr
-	r_port := myHtons(int(C.int(addr.sin_port)))//need myHtons to get correct remote port
-	C.inet_ntop(syscall.AF_INET,unsafe.Pointer(&addr.sin_addr),buffer_ptr,INET_ADDRSTRLEN)
-	r_ip := net.ParseIP(C.GoString(buffer_ptr))
-	raddr := &net.UDPAddr{
-		IP: r_ip,
-		Port:int(r_port),
-	}
-	*/
-	raddr := csockToUdpaddr(addr)
+
+	raddr := csockToUdpaddr(addr)//return UDPAddr
 	return int(recv_length), raddr, nil
 }
 
@@ -113,7 +77,6 @@ func (conn *MyConn) ReadFromUDP(b []byte)(int,*net.UDPAddr,error){
 //memory to send->use getCptrOfByteData or use &b[0] directly
 func (conn *MyConn) WriteToUDP(b []byte, addr *net.UDPAddr) (int, error) {
 	var raddress C.struct_sockaddr_in
-	//var buffer_ptr *C.char
 	var success_send_length C.long
 	//get information from UDPAddr and set the data of sockaddr_in
 	raddress = udpaddrToCsock(addr)
@@ -165,22 +128,22 @@ func udpaddrToCsock(addr *net.UDPAddr) (address C.struct_sockaddr_in){
 	if ip =="0.0.0.0"{
 		ip = "0"
 	}
-	Cip := C.CString(ip)
+	Cip := C.CString(ip)//allocate memory and copy ip !!(not buf is ok)
 	defer C.free(unsafe.Pointer(Cip))
 	port := addr.Port
 	address.sin_family = syscall.AF_INET
-	address.sin_addr.s_addr = C.inet_addr(Cip)//need to free???
+	address.sin_addr.s_addr = C.inet_addr(Cip)//need to free???yes-> free Cip
 	address.sin_port = C.ushort(myHtons(port))
 	return
 }
 //get the remote address and put it into UDPAddr
 func csockToUdpaddr(address C.struct_sockaddr_in) (addr *net.UDPAddr){
-	var buffer string
-	buffer_ptr := C.CString(buffer)
-	defer C.free(unsafe.Pointer(buffer_ptr))
+	var ip_buf string
+	ip_bufptr := C.CString(ip_buf)
+	defer C.free(unsafe.Pointer(ip_bufptr))
 	r_port := myHtons(int(C.int(address.sin_port)))//need myHtons to get correct remote port
-	C.inet_ntop(syscall.AF_INET,unsafe.Pointer(&address.sin_addr),buffer_ptr,INET_ADDRSTRLEN)
-	r_ip := net.ParseIP(C.GoString(buffer_ptr))
+	C.inet_ntop(syscall.AF_INET,unsafe.Pointer(&address.sin_addr),ip_bufptr,INET_ADDRSTRLEN)
+	r_ip := net.ParseIP(C.GoString(ip_bufptr))
 	addr = &net.UDPAddr{
 		IP: r_ip,
 		Port:int(r_port),
